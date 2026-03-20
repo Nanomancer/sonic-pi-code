e11457fa2e138eb69664fe91f4134f3c3205317a
Nanomancer <nanomancer.uk@gmail.com>
2026-03-20 18:35:18 +0000

    session 3, part 2, best time 00:30

--- practise-sessions/liveset_1/prac_log.rb
+++ practise-sessions/liveset_1/prac_log.rb
@@ -1,15 +1,19 @@
 ### session 3
 
 ### part 1: first sound - drone, next target: < 00:30
-## N/A
+### attempt 1, time 00:44 # - used different loop
+### attempt 2, time 00:36
+### attempt 3, time 00:36
+### attempt 4, time 00:30
+### attempt 5, time 00:32
+### attempt 6, time 00:35
 
 ### part 2: set up clock & syncs next target: < 00:30
-### I forgot to set up sync in drone for this session!
-### attempt 1, time 00:
-### attempt 2, time 00:
-### attempt 3, time 00:
-### attempt 4, time 00:
-### attempt 5, time 00:
+### attempt 1, time 00:31
+### attempt 2, time 00:35
+### attempt 3, time 00:36
+### attempt 4, time 00:34
+### attempt 5, time 00:30
 
 ### part 3: add beep (will become tech arp or lead)
 
