99247ea7ea774dbff0d5d6b6f59592111f179c22
Nanomancer <nanomancer.uk@gmail.com>
2026-03-20 10:42:01 +0000

    reset, session 3

--- practise-sessions/liveset_1/prac_log.rb
+++ practise-sessions/liveset_1/prac_log.rb
@@ -5,11 +5,11 @@
 
 ### part 2: set up clock & syncs next target: < 00:30
 ### I forgot to set up sync in drone for this session!
-### attempt 1, time 00:50
-### attempt 2, time 00:43
-### attempt 3, time 00:45
-### attempt 4, time 00:44
-### attempt 5, time 00:35
+### attempt 1, time 00:
+### attempt 2, time 00:
+### attempt 3, time 00:
+### attempt 4, time 00:
+### attempt 5, time 00:
 
 ### part 3: add beep (will become tech arp or lead)
 
