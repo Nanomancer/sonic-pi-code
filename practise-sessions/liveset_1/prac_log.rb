1ffbd628afdba53306a7e1ca8d2098e8c963060f
Nanomancer <nanomancer.uk@gmail.com>
2026-03-22 17:38:41 +0000

    session 4, parts 1 & 2

--- practise-sessions/liveset_1/prac_log.rb
+++ practise-sessions/liveset_1/prac_log.rb
@@ -1,19 +1,19 @@
-### session 3
+### session 4
 
-### part 1: first sound - drone, next target: < 00:30
-### attempt 1, time 00:44 # - used different loop
-### attempt 2, time 00:36
-### attempt 3, time 00:36
-### attempt 4, time 00:30
-### attempt 5, time 00:32
-### attempt 6, time 00:35
-
-### part 2: set up clock & syncs next target: < 00:30
-### attempt 1, time 00:31
+### part 1: first sound - drone, next target: < 00:30 for five runs
+### attempt 1, time 00:41
 ### attempt 2, time 00:35
-### attempt 3, time 00:36
-### attempt 4, time 00:34
-### attempt 5, time 00:30
+### attempt 3, time 00:26 !!!
+### attempt 4, time 00:28
+### attempt 5, time 00:25
+### attempt 6, time 00:30
+
+### part 2: set up clock & syncs next target: < 00:30 for five runs
+### attempt 1, time 00:34
+### attempt 2, time 00:33
+### attempt 3, time 00:30
+### attempt 4, time 00:27
+### attempt 5, time 00:39
 
 ### part 3: add beep (will become tech arp or lead)
 
